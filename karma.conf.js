cbd60f77aebd92290d00ee1bd75f916db64880ab
Sergio Rodríguez <alu0100699968@ull.edu.es>
2016-04-13 14:05:30 +0100

    Arreglando Karma y errores en tests, closed #2

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'public/tests/calculate-nomodule.js',
+      'public/tests/chai.js',
       'public/tests/*.js'
     ],
 
--- public/tests/calculate-nomodule.js
+++ public/tests/calculate-nomodule.js
@@ -1,42 +1,42 @@
 (function(exports) {
   "use strict";
 
-  const regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g;
-  const calculate = (original) => {
-    let lines = original.split(/\n+\s*/);
+  var regexp = /"((?:[^"\\]|\\.)*)"|([^,\s]+)|,\s*(?=,|$)|^\s*,/g;
+  var calculate = function(original) {
+    var lineas = original.split(/\n+\s*/);
 
-    for(var i in lines)
-      if(lines[i].match(regexp)) {
-        var commonLength = lines[i].match(regexp).length;
+    for(var i in lineas)
+      if(lineas[i].match(regexp)) {
+        var commonLength = lineas[i].match(regexp).length;
         break;
       }
 
-    let r = [];
-    const removeQuotes = (field) => {
+    var r = [];
+    var removeQuotes = function(field) {
       return field.replace(/,\s*$/, '').
       replace(/^\s*"/, '').
       replace(/"\s*$/, '').
       replace(/\\"/, '"');
     };
 
-    for (let t in lines) {
-      let temp = lines[t];
-      let m = temp.match(regexp);
-      let result = [];
-      let error = false;
+    for (var t in lineas) {
+      var temp = lineas[t];
+      var m = temp.match(regexp);
+      var result = [];
+      var error = false;
 
-      // skip empty lines and comments
+      // skip empty lineas and comments
       if (temp.match(/(^\s*$)|(^#.*)/)) continue;
       if (m) {
         result = m.map(removeQuotes);
         error = (commonLength != m.length);
-        let rowclass = error ? 'error' : 'legal';
+        var rowclass = error ? 'error' : 'legal';
         r.push({
           items: result,
           type: rowclass
         });
       } else {
-        let errmsg = 'La fila "' + temp + '" no es un valor de CSV permitido.';
+        var errmsg = 'La fila "' + temp + '" no es un valor de CSV permitido.';
         r.push({
           items: errmsg.split("").splice(commonLength),
           type: 'error'
