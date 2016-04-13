68f01775bcb5dcc8885d1e0785c1ecefb3b2c57c
Jonathan Expósito <alu0100696455@ull.edu.es>
2016-04-13 10:51:59 +0100

    Añadida dependencia y modificado calculate.js

--- models/calculate.js
+++ models/calculate.js
@@ -6,13 +6,6 @@
     let lines = original.split(/\n+\s*/);
     let commonLength = lines[0].match(regexp).length;
 
-    for (var i in lines)
-      if (lines[i].match(regexp)) {
-        firstLine = i;
-        var commonLength = lines[i].match(regexp).length;
-        break;
-      }
-
     let r = [];
     const removeQuotes = (field) => {
       return field.replace(/,\s*$/, '').
@@ -26,16 +19,13 @@
       let m = temp.match(regexp);
       let result = [];
       let error = false;
-      let first = false;
 
       // skip empty lines and comments
       if (temp.match(/(^\s*$)|(^#.*)/)) continue;
       if (m) {
         result = m.map(removeQuotes);
         error = (commonLength != m.length);
-        first = (firstLine == t);
         let rowclass = error ? 'error' : 'legal';
-        rowclass = first ? 'first' : rowclass;
         r.push({
           items: result,
           type: rowclass
