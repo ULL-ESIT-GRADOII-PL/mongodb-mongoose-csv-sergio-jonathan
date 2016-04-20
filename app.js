5f336d226aa555acb3251b56872561419721a895
Jonathan Expósito <alu0100696455@ull.edu.es>
2016-04-20 17:56:42 +0100

    Método find en app.js y añadida clase hidden al css

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-    
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,49 +18,58 @@ app.use(express.static(__dirname + '/public'));
 const calculate = require('./models/calculate');
 
 app.get('/', (request, response) => {
-  response.render('index', { title: 'Analizador CSV' });
+  response.render('index', {
+    title: 'Analizador CSV'
+  });
 });
 
 app.get('/csv', (request, response) => {
-  response.send({ "rows": calculate(request.query.input) });
+  response.send({
+    "rows": calculate(request.query.input)
+  });
 });
 
-app.param('entrada', function (req, res, next, entrada) {  
-  if (entrada.match(/^[a-z_]\w*\.csv$/i)) { 
-      req.entrada = entrada;
-  } else { 
-      next(new Error(`<${entrada}> no casa con los requisitos de 'entrada'`));
-   }
+app.param('entrada', function(req, res, next, entrada) {
+  if (entrada.match(/^[a-z_]\w*\.csv$/i)) {
+    req.entrada = entrada;
+  } else {
+    next(new Error(`<${entrada}> no casa con los requisitos de 'entrada'`));
+  }
   next();
 });
 
 const Input = require('./models/db-structure');
 
-app.get('/mongo/:entrada', function(req, res) { 
+app.get('/mongo/:entrada', function(req, res) {
   console.log(req.entrada);
-  
+
   mongoose.connect('mongodb://localhost/csv');
 
   let input = new Input({
-        "name": req.entrada,
-        "content": "test"
-    });
-    
-    let promise = input.save(function(err) {
-        if (err) {
-            console.log(`Hubieron errores:\n${err}`);
-            return err;
-        }
-        console.log(`Saved: ${input}`);
-    });
-    
-    Promise.resolve(promise).then((value) => {
-      console.log(value);
-      mongoose.connection.close();
+    "name": req.entrada,
+    "content": "test"
+  });
 
-    });
+  let promise = input.save(function(err) {
+    if (err) {
+      console.log(`Hubieron errores:\n${err}`);
+      return err;
+    }
+    console.log(`Saved: ${input}`);
+  });
+
+  Promise.resolve(promise).then((value) => {
+    console.log(value);
+    mongoose.connection.close();
+
+  });
+});
+
+app.get('/find', function(req, res) {
+  if (Input.find()[req.query.indice] != null)
+    req.query.elemento.toggleClass('hidden');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(`Node app is running at localhost: ${app.get('port')}` );
+  console.log(`Node app is running at localhost: ${app.get('port')}`);
 });
